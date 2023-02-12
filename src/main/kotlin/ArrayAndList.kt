d52955ddcea1f76dfb60c16778795c0e6239c4dc
whdcks420 <whdcks420@gmail.com>
2023-02-12 16:10:54 +0900

    Loop 사용법

--- src/main/kotlin/ArrayAndList.kt
+++ src/main/kotlin/ArrayAndList.kt
@@ -1,9 +1,11 @@
+fun main() {
+    forAndWhile()
+}
+
 // 5. Array and List
 // Array
-
-
 // List 1. List 2. MutableList
-fun main() {
+fun array() {
     val array = arrayOf(1, 2, 3)
     val list = listOf(1, 2, 3)
 
@@ -14,4 +16,36 @@ fun main() {
     arrayList.add(10)
     arrayList.add(20)
     arrayList[0] = 20
+}
+
+fun forAndWhile() {
+    val students = arrayListOf("kim", "jong", "chan")
+
+    for ((index, name) in students.withIndex() ) {
+        println("${index + 1}번째 학생의 이름 : ${name}")
+    }
+
+    for (name in students) {
+        println("${name}")
+    }
+
+    var sum = 0
+    for (i in 1..10) {
+        sum += i
+    }
+    println(sum)
+
+    for (i in 1..10 step 2) {
+    }
+    for (i in 10 downTo 1) {
+    }
+    for (i in 1 until 100) { // 1..99
+
+    }
+
+    var index = 0
+    while (index < 10) {
+        println("current index : ${index}")
+        index++
+    }
 }
