e46e361f11de81d2dcc5a6831a95d5622aac72ad
whdcks420 <whdcks420@gmail.com>
2023-02-12 15:59:17 +0900

    array, list(list, mutablelist)

---
+++ src/main/kotlin/ArrayAndList.kt
@@ -0,0 +1,17 @@
+// 5. Array and List
+// Array
+
+
+// List 1. List 2. MutableList
+fun main() {
+    val array = arrayOf(1, 2, 3)
+    val list = listOf(1, 2, 3)
+
+    val array2 = arrayOf(1, "a", 3.4)
+    val list2 = listOf(1, "a", 3.4)
+
+    val arrayList = arrayListOf<Int>()
+    arrayList.add(10)
+    arrayList.add(20)
+    arrayList[0] = 20
+}
